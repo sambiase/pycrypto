588f7c2a6324d965d42e36fad873b84abff8fc96
4n6ics <marsamb@gmail.com>
2021-08-03 16:26:23 -0300

    Codecov added to the project
    close #3

---
+++ marginTrading/tests/test_spam/test_enviador_de_email.py
@@ -0,0 +1,11 @@
+from marginTrading.spam.enviador_de_email import Enviador
+
+
+def test_criar_enviador_de_email():
+    enviador = Enviador()
+    assert enviador is not None
+
+def test_remetente():       #verifica se remetente do email se encontra no conteudo
+    enviador = Enviador()
+    resultado = enviador.enviar('marsamb@gmail.com','eth0.cybersec@gmail.com','Curso Python Pro','Excelente curso de Python')
+    assert 'marsamb@gmail.com' in resultado
