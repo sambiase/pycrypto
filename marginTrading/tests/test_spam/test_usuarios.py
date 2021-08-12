deac39f1c5a709b9ea09c19cd87d4c09f1b73c9a
4n6ics <marsamb@gmail.com>
2021-08-12 14:34:02 -0300

    Added sysInfo

--- marginTrading/tests/test_spam/test_usuarios.py
+++ marginTrading/tests/test_spam/test_usuarios.py
@@ -2,14 +2,14 @@ from marginTrading.spam.modelos import Usuario
 
 
 def test_salvar_usuario(sessao):
-    usuario = Usuario(nome='Andre')
+    usuario = Usuario(nome='Andre',email='andreteste@gmail.com')
     sessao.salvar(usuario)
     assert isinstance(usuario.id,int)   # certifica que o usuario possui um ID e que é instancia do tipo int
 
 
 
 def test_listar_usuario(sessao):
-    usuarios = [Usuario(nome='Andre'), Usuario(nome='Flavio')]
+    usuarios =
     for usuario in usuarios:            # salvar todos os usuarios na Lista
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()  # certifica que o usuario esta na lista de usuarios
