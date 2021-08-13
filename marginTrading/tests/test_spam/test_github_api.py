774f6e65a0da68f249abfac0cec8a5759155760f
4n6ics <marsamb@gmail.com>
2021-08-13 19:28:35 -0300

    Added @fixture

--- marginTrading/tests/test_spam/test_github_api.py
+++ marginTrading/tests/test_spam/test_github_api.py
@@ -5,15 +5,15 @@ from marginTrading import github_api
 
 
 @pytest.fixture
-def avatar_url():
+def avatar_url(mocker):
     resp_mock = Mock()
     url = 'https://avatars.githubusercontent.com/u/78605825?v=4'
     resp_mock.json.return_value = {'login': 'sambiase', 'id': 78605825, 'node_id': 'MDQ6VXNlcjc4NjA1ODI1',
                                    'avatar_url': url}
-    get_original = github_api.requests.get
-    github_api.requests.get = Mock(return_value=resp_mock)
-    yield url
-    github_api.requests.get = get_original
+    get_mock = mocker.patch('marginTrading.github_api.requests.get')
+    get_mock.return_value = resp_mock
+    return url
+
 
 
 def teste_buscar_avatar(avatar_url):
