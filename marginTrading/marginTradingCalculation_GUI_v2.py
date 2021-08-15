da92106901f34e190ed44883862b7b72bb2ff2f7
4n6ics <marsamb@gmail.com>
2021-08-15 18:21:39 -0300

    Deleted semicolon

--- marginTrading/marginTradingCalculation_GUI_v2.py
+++ marginTrading/marginTradingCalculation_GUI_v2.py
@@ -1,8 +1,8 @@
 '''
-    MarginTrading Calc GUI - Tkinter TTK
+    MarginTrading Calc GUI - ttkinter ttk
 '''
 from tkinter import FALSE, TOP, LEFT, RIGHT
-import tkinter as tk
+import tkinter as ttk
 from ttkbootstrap import Style
 from tkinter import ttk
 
@@ -15,9 +15,10 @@ class MarginTradingCalcGui:
         """
         style = Style(theme='superhero')
         self.window = style.master
-        self.window.configure('TButton', font=('Helvetica', 12))
+        style.configure('TButton', font=('Gotham', 14))
+        style.configure('TEntry', fieldbackground='white')
         self.window.title('MarginTrading Calc v1')
-        self.window.geometry('350x440')
+        self.window.geometry('340x430')
         self.window.resizable(FALSE, FALSE)
         self.window.iconbitmap('CryptoTech_Logo.ico')
 
@@ -46,54 +47,54 @@ class MarginTradingCalcGui:
         :return:
         """
 
-        top_frame = tk.Frame(self.window)
+        top_frame = ttk.Frame(self.window)
         colateral_frame = ttk.Frame(self.window)
-        leverage_frame = tk.Frame(self.window)
-        target_frame = tk.Frame(self.window)
-        stop_frame = tk.Frame(self.window)
+        leverage_frame = ttk.Frame(self.window)
+        target_frame = ttk.Frame(self.window)
+        stop_frame = ttk.Frame(self.window)
         calc_btn_frame = ttk.Frame(self.window)
 
-        colateral_text = ttk.Label(colateral_frame, text='Valor Colateral', width=15,
-                                   foreground='white',font='Gotham 15 bold')
-        colateral_input = ttk.Entry(colateral_frame, width=5,font='Gotham 13 ')
+        colateral_text = ttk.Label(colateral_frame, text='Valor Colateral', width=15, anchor='c', foreground='black',
+                                   background='#F0B90B', font='Gotham 14 bold')
+        colateral_input = ttk.Entry(colateral_frame)
 
-        leverage_text = tk.Label(leverage_frame, text='Alavancagem (x)', width=18, height=2, fg='black',
-                                 bg='#F0B90B',font='Gotham 15 bold')
-        leverage_input = tk.Entry(leverage_frame, bg='white', font='Gotham 13 ')
+        leverage_text = ttk.Label(leverage_frame, text='Alavancagem (x)', width=15, anchor='c', foreground='black',
+                                  background='#F0B90B', font='Gotham 14 bold')
+        leverage_input = ttk.Entry(leverage_frame)
 
-        target_text = tk.Label(target_frame, text='Alvo (%)', width=18, height=2, fg='black', bg='#F0B90B',
-                               font='Gotham 15 bold')
-        target_input = tk.Entry(target_frame, bg='white', font='Gotham 13 ')
+        target_text = ttk.Label(target_frame, text='Alvo (%)', width=15, anchor='c', foreground='black',
+                                background='#F0B90B', font='Gotham 14 bold')
+        target_input = ttk.Entry(target_frame)
 
-        stop_text = tk.Label(stop_frame, text='Stop (%)', width=18, height=2, fg='black', bg='#F0B90B',
-                             font='Gotham 15 bold')
-        stop_input = tk.Entry(stop_frame, bg='white', font='Gotham 13 ')
+        stop_text = ttk.Label(stop_frame, text='Stop (%)', width=15, anchor='c', foreground='black',
+                              background='#F0B90B', font='Gotham 14 bold')
+        stop_input = ttk.Entry(stop_frame)
 
-        calc_btn = ttk.Button(calc_btn_frame, text='Calcular', style='link.TButton', width=20,
-                             command=lambda: MarginTradingCalcGui.res_window(None, int(colateral_input.get()),
-                                                                             int(leverage_input.get()),
-                                                                             int(target_input.get()),
-                                                                             int(stop_input.get())))
+        calc_btn = ttk.Button(calc_btn_frame, text='Calcular', style='secondary.TButton', width=10,
+                              command=lambda: MarginTradingCalcGui.res_window(None, int(colateral_input.get()),
+                                                                              int(leverage_input.get()),
+                                                                              int(target_input.get()),
+                                                                              int(stop_input.get())))
 
         top_frame.pack(pady=15)
 
-        colateral_frame.pack(pady=15)
-        colateral_text.pack(side=LEFT)
-        colateral_input.pack(padx=15)
+        colateral_frame.pack(side=TOP, pady=15)
+        colateral_text.pack(side=LEFT, ipadx=25, ipady=10)
+        colateral_input.pack(side=RIGHT, padx=30, ipady=3)
 
         leverage_frame.pack(side=TOP, pady=15)
-        leverage_text.pack(side=LEFT)
-        leverage_input.pack(side=RIGHT, padx=35, ipady=3)
+        leverage_text.pack(side=LEFT, ipadx=25, ipady=10)
+        leverage_input.pack(side=RIGHT, padx=30, ipady=3)
 
         target_frame.pack(side=TOP, pady=15)
-        target_text.pack(side=LEFT)
-        target_input.pack(side=RIGHT, padx=35, ipady=3)
+        target_text.pack(side=LEFT, ipadx=25, ipady=10)
+        target_input.pack(side=RIGHT, padx=30, ipady=3)
 
         stop_frame.pack(side=TOP, pady=15)
-        stop_text.pack(side=LEFT)
-        stop_input.pack(side=RIGHT, padx=35, ipady=3)
+        stop_text.pack(side=LEFT, ipadx=25, ipady=10)
+        stop_input.pack(side=RIGHT, padx=30, ipady=3)
 
-        calc_btn_frame.pack(pady=20)
+        calc_btn_frame.pack(pady=30)
 
         calc_btn.pack()
 
@@ -102,10 +103,9 @@ class MarginTradingCalcGui:
             Window that shows the results of the Margin Calculation
         :return:
         """
-
-        res_window = tk.Tk()
-        res_window.title('MarginTrading Calc v1 - Results')
-        res_window.configure(bg='black')
+        style = Style(theme='cyborg')
+        res_window = style.master
+        res_window.title('MarginTrading Calc v2 - Results')
         res_window.geometry('350x440')
         res_window.resizable(FALSE, FALSE)
         res_window.iconbitmap('CryptoTech_Logo.ico')
@@ -113,57 +113,57 @@ class MarginTradingCalcGui:
         calc_results = MarginTradingCalcGui.mt_calculation(None, colateral_input, alavancagem_input, target_input,
                                                            stop_input)
 
-        top_frame = tk.Frame(res_window)
-        colateral_frame = tk.Frame(res_window, bg='black')
-        leverage_frame = tk.Frame(res_window, bg='black')
-        loan_frame = tk.Frame(res_window, bg='black')
-        trade_total_value_frame = tk.Frame(res_window, bg='black')
-        liquidation_percent_frame = tk.Frame(res_window, bg='black')
-        liquidation_value_frame = tk.Frame(res_window, bg='black')
-        profit_frame = tk.Frame(res_window, bg='black')
-        loss_frame = tk.Frame(res_window, bg='black')
+        top_frame = ttk.Frame(res_window)
+        colateral_frame = ttk.Frame(res_window)
+        leverage_frame = ttk.Frame(res_window)
+        loan_frame = ttk.Frame(res_window)
+        trade_total_value_frame = ttk.Frame(res_window)
+        liquidation_percent_frame = ttk.Frame(res_window)
+        liquidation_value_frame = ttk.Frame(res_window)
+        profit_frame = ttk.Frame(res_window)
+        loss_frame = ttk.Frame(res_window)
 
-        colateral_text = tk.Label(colateral_frame, text='Valor Colateral', width=18, fg='black', bg='#F0B90B',
-                                  font='Gotham 14 bold')
+        colateral_text = ttk.Label(colateral_frame, text='Valor Colateral', width=15, anchor='c', foreground='black',
+                                   background='#F0B90B', font='Gotham 14 bold')
 
-        colateral_res = tk.Label(colateral_frame, text=f'{colateral_input}', width=18, bg='white', font='Gotham 13')
+        colateral_res = ttk.Label(colateral_frame, text=f'{colateral_input}', width=18, font='Gotham 13')
 
-        leverage_text = tk.Label(leverage_frame, text='Alavancagem (x)', width=18, fg='black', bg='#F0B90B',
-                                 font='Gotham 14 bold')
+        leverage_text = ttk.Label(leverage_frame, text='Alavancagem (x)', width=15, anchor='c', foreground='black',
+                                   background='#F0B90B', font='Gotham 14 bold')
 
-        leverage_res = tk.Label(leverage_frame, text=f'{alavancagem_input}', width=18, bg='white', font='Gotham 13')
+        leverage_res = ttk.Label(leverage_frame, text=f'{alavancagem_input}', width=18, font='Gotham 13')
 
-        loan_text = tk.Label(loan_frame, text='Valor do Empréstimo', width=18, fg='black', bg='#F0B90B',
-                             font='Gotham 14 bold')
+        loan_text = ttk.Label(loan_frame, text='Valor do Empréstimo', width=15, anchor='c', foreground='black',
+                                   background='#F0B90B', font='Gotham 14 bold')
 
-        loan_res = tk.Label(loan_frame, text=f'{calc_results[0]}', width=18, bg='white', font='Gotham 13')
+        loan_res = ttk.Label(loan_frame, text=f'{calc_results[0]}', width=18, font='Gotham 13')
 
-        trade_total_value_text = tk.Label(trade_total_value_frame, text='Valor Total do Trade', width=18, fg='black',
-                                          bg='#F0B90B',
-                                          font='Gotham 14 bold')
+        trade_total_value_text = ttk.Label(trade_total_value_frame, text='Valor Total do Trade', width=15, anchor='c', foreground='black',
+                                   background='#F0B90B', font='Gotham 14 bold')
 
-        trade_total_value_res = tk.Label(trade_total_value_frame, text=f'{calc_results[1]}', width=18, bg='white',
-                                         font='Gotham 13')
+        trade_total_value_res = ttk.Label(trade_total_value_frame, text=f'{calc_results[1]}', width=18, font='Gotham 13')
 
-        liquidation_percent_text = tk.Label(liquidation_percent_frame, text='Liquidação (%)', width=18, fg='black',
-                                            bg='#F0B90B', font='Gotham 14 bold')
+        liquidation_percent_text = ttk.Label(liquidation_percent_frame, text='Liquidação (%)', width=15, anchor='c', foreground='black',
+                                   background='#F0B90B', font='Gotham 14 bold')
 
-        liquidation_percent_res = tk.Label(liquidation_percent_frame, text=f'{round(calc_results[2] * 100, 2)}',
-                                           width=18, bg='white', font='Gotham 13')
+        liquidation_percent_res = ttk.Label(liquidation_percent_frame, text=f'{round(calc_results[2] * 100, 2)}',
+                                            width=18, font='Gotham 13')
 
-        liquidation_value_text = tk.Label(liquidation_value_frame, text='Valor de Liquidação', width=18, fg='black',
-                                          bg='#F0B90B', font='Gotham 14 bold')
+        liquidation_value_text = ttk.Label(liquidation_value_frame, text='Valor de Liquidação', width=15, anchor='c', foreground='black',
+                                   background='#F0B90B', font='Gotham 14 bold')
 
-        liquidation_value_res = tk.Label(liquidation_value_frame, text=f'{round(calc_results[3], 2)}', width=18,
-                                         bg='white', font='Gotham 13')
+        liquidation_value_res = ttk.Label(liquidation_value_frame, text=f'{round(calc_results[3], 2)}', width=18,
+                                          font='Gotham 13')
 
-        profit_text = tk.Label(profit_frame, text='Lucro', width=18, fg='black', bg='#F0B90B', font='Gotham 14 bold')
+        profit_text = ttk.Label(profit_frame, text='Lucro', width=15, anchor='c', foreground='black',
+                                   background='#F0B90B', font='Gotham 14 bold')
 
-        profit_res = tk.Label(profit_frame, text=f'{round(calc_results[4], 2)}', width=18, bg='white', font='Gotham 13')
+        profit_res = ttk.Label(profit_frame, text=f'{round(calc_results[4], 2)}', width=18, font='Gotham 13')
 
-        loss_text = tk.Label(loss_frame, text='Prejuízo', width=18, fg='black', bg='#F0B90B', font='Gotham 14 bold')
+        loss_text = ttk.Label(loss_frame, text='Prejuízo', width=15, anchor='c', foreground='black',
+                                   background='#F0B90B', font='Gotham 14 bold')
 
-        loss_res = tk.Label(loss_frame, text=f'{round(calc_results[5], 2)}', width=18, bg='white', font='Gotham 13')
+        loss_res = ttk.Label(loss_frame, text=f'{round(calc_results[5], 2)}', width=18, font='Gotham 13')
 
         top_frame.pack(pady=15)
 
