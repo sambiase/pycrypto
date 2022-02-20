c6288dab685ba79f03e7b6a3f071f89217e26261
Martinho Sambiase <martinho.sambiase@dremio.com>
2022-02-20 10:16:06 -0300

    deleted files

--- marginTrading/spam/main.py
+++
@@ -1,8 +0,0 @@
-class EnviadorDeSpam:
-    def __init__(self,sessao, enviador):
-        self.enviador = enviador
-        self.sessao = sessao
-
-    def enviar_emails(self, remetente, assunto, corpo):
-        for usuario in self.sessao.listar():             #para cada usuario no BD
-            self.enviador.enviar(remetente,usuario.email,assunto,corpo)
--- marginTrading/spam/modelos.py
+++
@@ -1,5 +0,0 @@
-class Usuario():
-    def __init__(self, nome, email): 
-        self.email = email
-        self.nome = nome
-        self.id = None                  # ID inicial do Obj criado no BD
